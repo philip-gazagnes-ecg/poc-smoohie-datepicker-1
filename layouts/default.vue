6c7fe73c6382ac0bdc5abd55cea5639b72a983b5
Philip <p.gazagnes@vacanceselect.com>
2023-11-08 15:24:49 +0100

    compare mobile 4 - 18 months

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,8 +4,11 @@
       <nuxt-link to="/device-desktop" class="block h-16 bg-blue-400 hover:bg-blue-300 rounded flex items-center justify-center text-xs">
         Desktop
       </nuxt-link>
-      <nuxt-link to="/device-mobile" class="block h-16 bg-blue-400 hover:bg-blue-300 mt-1 rounded flex items-center justify-center text-xs">
-        Mobile
+      <nuxt-link to="/device-mobile-4" class="block h-16 bg-blue-400 hover:bg-blue-300 mt-1 rounded flex items-center justify-center text-xs">
+        Mobile (4 months)
+      </nuxt-link>
+      <nuxt-link to="/device-mobile-18" class="block h-16 bg-blue-400 hover:bg-blue-300 mt-1 rounded flex items-center justify-center text-xs">
+        Mobile (18 months)
       </nuxt-link>
     </div>
     <div class="flex-1 h-full">
